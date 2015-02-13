28e5f21c9e8ed9995d452a8b5cb5c933fd2696a7
liang <angli@thoughtworks.com>
2015-02-13 10:14:40 +0800

    finish updateWeather

--- app/src/main/java/com/liulishuo/llsdemo/MainActivity.java
+++ app/src/main/java/com/liulishuo/llsdemo/MainActivity.java
@@ -2,23 +2,29 @@ package com.liulishuo.llsdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.liulishuo.llsdemo.Model.WEATHER;
 import com.liulishuo.llsdemo.Services.WeatherService;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final int CITY_ID = 101010100;
+    public static final int CITY_ID = 101010100;
+
+    @InjectView(R.id.cityWeather_tv)
+    TextView cityWeather_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
         updateWeather();
     }
 
@@ -26,10 +32,20 @@ public class MainActivity extends ActionBarActivity {
         WeatherService weatherService = new WeatherService();
         weatherService.getWeather(CITY_ID)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<WEATHER>() {
+                .subscribe(new Subscriber<WEATHER>() {
                     @Override
-                    public void call(WEATHER weather) {
-                        Log.e("", "city:" + weather.getCity());
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(WEATHER weather) {
+                        cityWeather_textView.setText(weather.getCity()+":"+ weather.getWeather());
                     }
                 });
     }
